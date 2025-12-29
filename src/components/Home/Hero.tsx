4accb61de656dfb03fff6e11753a7d516810a94b
Aman <amngupta402@gmail.com>
2025-12-30 04:01:02 +0530

    hero

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,16 +1,17 @@
 export default function Hero() {
   return (
-    <section className="min-h-svh max-h-screen w-full flex items-center justify-center relative">
+    <>
+    <section className="h-full w-full relative">
       <img
-        src="/hero/frame.svg"
+        src="/hero/frame.png"
         alt=""
-        className="w-full absolute top-0 aspect-[16/9]"
+        className="w-full absolute top-0 "
       />
-      <img
-        src="/hero/pixel_layer.svg"
-        alt=""
-        className="w-full absolute -bottom-30 aspect-[16/9]"
-      />
+      <img src="/hero/pixel_layer.png" alt="" className="w-full absolute top-10" />
     </section>
+    <section className="h-[700px] bg-[#3054e5]">
+
+    </section>
+    </>
   );
 }
