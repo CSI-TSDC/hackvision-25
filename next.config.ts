3f749a9e1aab984751f7e204d5ed7659b14ef059
realhardik <hardik.dev.08@gmail.com>
2026-01-21 14:06:09 +0530

    retry rewrite

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   assetPrefix: "/hackvision",
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/hackvision",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
